db2e07d115f8ece7b947f1a3ed6a8d9bb82a8d35
Druhin Tarafder <druhinpro@gmail.com>
2020-04-20 15:39:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const api_url = 'emoji.json';
 
+async function getData() {
+	const response = await fetch(api_url);
+
+	const data = await response.json();
+
+	//	document.getElementById("h1").innerHTML = (data);
+
+	//	console.log(data);
+
+	//	console.log(data.a);
+	//	console.log(data.b);
+
+
+	console.log(data.a[" ".charCodeAt(0)]);
+	console.log(data.b[" ".charCodeAt(0)]);
+
+
+}
+
+getData();
